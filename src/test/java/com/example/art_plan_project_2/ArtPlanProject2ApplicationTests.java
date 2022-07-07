0b2079a4e590e4134f5ad0cf396c4b22b92461f9
BashirovAlfit <alfit.bashirov@gmail.com>
2022-07-07 13:18:50 +0600

    #1 Initial commit

---
+++ src/test/java/com/example/art_plan_project_2/ArtPlanProject2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.art_plan_project_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtPlanProject2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
